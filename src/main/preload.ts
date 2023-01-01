5b4b332e7e9cf533fb899f4adc8679e41ef94154
Juan <harusurv@hotmail.com>
2023-01-01 21:06:23 +0100

    sad

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -21,6 +21,6 @@ contextBridge.exposeInMainWorld('electron', {
     },
     once(channel: Channels, func: (...args: unknown[]) => void) {
       ipcRenderer.once(channel, (_event, ...args) => func(...args));
-    },
+    }
   },
 });
